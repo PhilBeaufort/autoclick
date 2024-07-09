680f61c7551695661f3dcc36918c234d236f6875
Philippe Beaufort <philippe.beaufort.1@ens.etsmtl.ca>
2024-07-08 23:40:49 -0400

    find button and click

--- autoclicker.py
+++ autoclicker.py
@@ -3,9 +3,22 @@ import pyautogui
 import time
 import keyboard
 
-def click(): 
-    time.sleep(0.1)     
-    pyautogui.click()
+def click_button(image_path, confidence=0.8):
+    # Locate the button on the screen
+    button_location = pyautogui.locateCenterOnScreen(image_path, confidence=confidence)
+    if button_location:
+        # Click the button
+        pyautogui.click(button_location)
+        return True
+    return False
 
+# Specify the path to your image
+button_image = 'slow_download.png'
+
+# Run the function
 while not keyboard.is_pressed('esc'):
-    click()
+    found = click_button(button_image)
+    if found:
+        print("Button clicked!")
+        break
+    time.sleep(0.5)  # Wait before trying again
